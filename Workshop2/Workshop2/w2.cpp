64bdf584dd587c0ab4d5e68cbac7e35262ca9593
H3ll0W0rld <kbpatel13@myseneca.ca>
2014-02-13 17:00:39 -0500

    Workshop2 Modified

--- Workshop2/Workshop2/w2.cpp
+++ Workshop2/Workshop2/w2.cpp
@@ -5,12 +5,12 @@
    School of Information & Communication Technology 
    E-mail: kbpatel13@myseneca.ca */
 
-//Workshop 1
-//C++ Function Version
+//Workshop 2
+//C++ Generic Pointers
 
 #include <iostream>
 #include <ctime>
-#define N 1000000000
+#define N 10000000
 
 void swap(void *a, void *b, int size) {
 	char *temp = (char*) malloc (sizeof(char)* size);	//defining character of incoming size
@@ -19,8 +19,14 @@ void swap(void *a, void *b, int size) {
 		memcpy(a, b, size);
 		memcpy(b, temp, size);
 	}
-	free(temp);
+	free(temp);						//free memory occupied by temp buffer
 }
+
+// Struct to test with int and doubles
+struct test {
+		int a, b;
+		double x, y;
+};
 
 int main()	{
 	std::cout << "File w2_cpp.cpp" << std::endl;	// Declaration of variable
@@ -29,32 +35,47 @@ int main()	{
 	double x = 2.2, y = 4.4;
 	short c = 3, d = 4;
 	unsigned long long v = 3.456, w = 2.345;
+	
 
 	c0 = clock();	//start timer
 	std::cout << "Processing integer values, please wait..." << std::endl;	
 	swap(&a, &b, 4);
-	c1 = clock();	//strt second timer
-	std::cout << "Process time for ints               is : " << (double)(c1-c0)/CLOCKS_PER_SEC << " secs" << std::endl;
+	c1 = clock();	//start second timer
+	std::cout << "Process time for ints               is : " << (double)(c1-c0)/CLOCKS_PER_SEC << " secs" << std::endl << std::endl;
 	
 	c0 = clock();	//start timer
 	std::cout << "Processing double values, Please wait..." << std::endl;	
 	swap(&x, &y, 8);
-	c1 = clock();	//strt second timer 
-	std::cout << "Process time for double             is :" << (double)(c1-c0)/CLOCKS_PER_SEC << " secs" << std::endl;
+	c1 = clock();	//start second timer 
+	std::cout << "Process time for double             is :" << (double)(c1-c0)/CLOCKS_PER_SEC << " secs" << std::endl << std::endl;
 
 	c0 = clock();	//start timer
 	std::cout << "Processing short values, Please wait..." << std::endl;	
 	swap(&c, &d, 2);
-	c1 = clock();	//strt second timer
-	std::cout << "Process time for short              is :" << (double)(c1-c0)/CLOCKS_PER_SEC << " secs" << std::endl;
+	c1 = clock();	//start second timer
+	std::cout << "Process time for short              is :" << (double)(c1-c0)/CLOCKS_PER_SEC << " secs" << std::endl << std::endl;
 
 	c0 = clock();	//start timer
 	std::cout << "Processing unsigned long long values, Please wait..." << std::endl;	
 	swap(&v, &w, 8);
-	c1 = clock();	//strt second timer
-	std::cout << "Process time for unsigned long long is :" << (double)(c1-c0)/CLOCKS_PER_SEC << " secs" << std::endl;
+	c1 = clock();	//start second timer
+	std::cout << "Process time for unsigned long long is :" << (double)(c1-c0)/CLOCKS_PER_SEC << " secs" << std::endl << std::endl;
+
+	test t; t.a = 2; t.b = 4; t.x = 2.2; t.y = 2.4;
+	c0 = clock();	//start timer
+	std::cout << "Processing Struct with Int values, Please wait..." << std::endl;	
+	swap(&t.a, &t.b, 8);
+	c1 = clock();	//start second timer
+	std::cout << "Process time for Struct with Int is :" << (double)(c1-c0)/CLOCKS_PER_SEC << " secs" << std::endl << std::endl;
+
+	c0 = clock();	//start timer
+	std::cout << "Processing Struct with Double values, Please wait..." << std::endl;	
+	swap(&t.x, &t.y, 8);
+	c1 = clock();	//start second timer
+	std::cout << "Process time for Struct with Double is :" << (double)(c1-c0)/CLOCKS_PER_SEC << " secs" << std::endl << std::endl;
+
 
 	std::cout << "Completed Now..." << std::endl;	
-	system("pause"); //prevent sudden execution end of program
+	system("pause"); //prevent sudden execution end of program in windows environment
 	return 0;
-}
+}		//End of main
